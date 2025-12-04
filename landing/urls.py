98893631b50f1a813b291fecb40a6d62005762c8
Kumasuun <88749531+Kumasuun@users.noreply.github.com>
2025-12-04 21:51:56 +0000

    Added authentication

--- landing/urls.py
+++ landing/urls.py
@@ -1,21 +1,23 @@
-
-
-
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+
+    # Static pages
     path('pricing/', views.pricing, name='pricing'),
     path('about/', views.about, name='about'),
     path('documentation/', views.documentation, name='documentation'),
-    
-    # Correct signup route
-    path('signup/', views.signup, name='signup'),
 
-    # Correct login route
+    # Auth
+    path('signup/', views.register, name='signup'),
     path('login/', views.login_view, name='login'),
-    
-    # OTP
-    path('verify-otp/', views.verify_otp, name='verify_otp'),
+
+    # Email OTP (first factor)
+    path("verify-otp/", views.verify_signup_otp, name="verify_signup_otp"),
+
+
+    # Google Authenticator (Second factor)
+    path('setup-2fa/', views.setup_2fa, name='setup_2fa'),
+    path('verify-2fa/', views.verify_2fa, name='verify_2fa'),
 ]
